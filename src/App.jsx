5f35c3cda7aab6253b7149e73847f8b3636429d9
Anurag Dangi <anuragdangi015@gmail.com>
2025-05-06 21:56:41 +0530

    setup tailwind+daisyUI+navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import NavBar from './NavBar';
+
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <NavBar />
+      <div className="p-8">
+        <h1 className="text-3xl font-bold mb-4">DaisyUI Test</h1>
+        <button className="btn btn-primary">Test Button</button>
+      </div>
     </>
   );
 }
---
+++ src/NavBar.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const themes = ['dark', 'light', 'aqua', 'valentine', 'coffee'];
+function NavBar() {
+  const [selectedTheme, setSelectedTheme] = useState(
+    localStorage.getItem('theme') || 'dark'
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', selectedTheme);
+    localStorage.setItem('theme', selectedTheme);
+  }, [selectedTheme]);
+
+  const handleThemeChange = (e) => {
+    setSelectedTheme(e.target.value);
+  };
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  return (
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">👩‍💻DevChat👨‍💻</a>
+      </div>
+      <div className="flex gap-2">
+        {/* this is the theme dropdown */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Theme
+            <svg
+              width="12px"
+              height="12px"
+              className="inline-block h-2 w-2 fill-current opacity-60"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="dropdown-content bg-base-300 rounded-box z-10 w-52 p-2 shadow-2xl"
+          >
+            {themes.map((theme) => (
+              <li key={theme}>
+                <label className="label cursor-pointer">
+                  <input
+                    type="radio"
+                    name="theme-dropdown"
+                    className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                    value={theme}
+                    checked={selectedTheme === theme}
+                    onChange={handleThemeChange}
+                    aria-label={capitalize(theme)}
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* this is the profile pic */}
+        <div className="dropdown dropdown-end mx-5">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
