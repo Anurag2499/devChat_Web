d9c8cf5acbb6fed8d6bac70d007388a0b0e5dece
Anurag Dangi <anuragdangi015@gmail.com>
2025-05-09 00:06:56 +0530

    login api integrated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('anurag@gmail.com');
+  const [password, setPassword] = useState('Anurag@123');
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        'http://localhost:7777/login',
+        {
+          emailId: email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex my-50 justify-center mx-20">
+      <div className="card bg-base-300 w-96">
+        <div className="card-body items-center text-center">
+          <h1 className="card-title">Login</h1>
+          <div>
+            <div className="form-control my-4">
+              <div className=" flex flex-row items-start">
+                <span className="label-text">Username</span>
+              </div>
+              <label className="input validator my-1">
+                <svg
+                  className="h-[1em] text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  required
+                  placeholder="Email/Username"
+                  // pattern="[A-Za-z][A-Za-`z0-9\-]*"
+                  minlength="4"
+                  maxlength="30"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  title="Only letters, numbers or dash"
+                  // className="text-gray-500"
+                />
+              </label>
+            </div>
+
+            <div className="form-control my-4">
+              <div className="flex flex-row items-start">
+                <span className="label-text">Password</span>
+              </div>
+              <label className="input validator my-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-[1em] text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 11c1.1 0 2-.9 2-2V7a2 2 0 00-4 0v2c0 1.1.9 2 2 2zm0 0v2m6 0a6 6 0 10-12 0v2a2 2 0 002 2h8a2 2 0 002-2v-2z"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  required
+                  placeholder="Password"
+                  // className="text-gray-500"
+                  className="text-"
+                  minlength="6"
+                  maxlength="30"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  title="Only letters, numbers or dash"
+                />
+              </label>
+            </div>
+          </div>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-outline btn-default"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
